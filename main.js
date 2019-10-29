1ed8ddf7f5b13383c234d001cde4386723bd4efe
Richard <richardkangg@gmail.com>
2019-10-29 09:54:34 -0400

    fixed header footer

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// const footer = document.body.querySelector('.footer');
+
+// document.body.onscroll = function() {
+//     const scroller = document.body.scrollTop();
+//     let position = scroll(0, 650);
+//     console.log(scroller);
+//     if (scroller > position) {
+//         footer.removeClass('hide');
+//     }
+//         else {footer.addClass('hide');
+
+//         }
+//     };
+
+const beverage = document.body.querySelector('#beverage');
+beverage.onClick = scroll(0, 610);
