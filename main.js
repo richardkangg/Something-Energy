e94470c4cd98ee35b93822174301e13c877da3c4
Richard <richardkangg@gmail.com>
2019-10-31 16:22:20 -0400

    added slides

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 
-const beverage = document.body.querySelector('#beverage');
-let link1 = document.getElementById("mark1").offsetTop;
-console.log(link1);
-beverage.onClick = scroll(0, link1);
+// const beverage = document.body.querySelector('#beverage');
+// let link1 = document.getElementById("mark1").offsetTop;
+// console.log(link1);
+// beverage.onClick = scroll(0, link1);
 // beverage.onClick = scroll(0, 536);
 
 window.onscroll = function() {hideFooter()};
@@ -16,4 +16,27 @@ const hideFooter = function() {
     document.getElementById("foot").className = "hide";
     document.getElementById("header2").className = "hideNav";
   }
+}
+
+const redcan = document.getElementById("redcan");
+
+const red = function() {
+    console.log("red can clicked");
+    document.getElementById("flavors").className = "hide";
+    document.getElementById("slides").className = "display";
+    document.querySelector(".slide1").className = "display";
+}
+
+const orange = function() {
+    console.log("orange can clicked");
+    document.getElementById("flavors").className = "hide";
+    document.getElementById("slides").className = "display";
+    document.querySelector(".slide2").className = "display";
+}
+
+const blue = function() {
+    console.log("blue can clicked");
+    document.getElementById("flavors").className = "hide";
+    document.getElementById("slides").className = "display";
+    document.querySelector(".slide3").className = "display";
 }
